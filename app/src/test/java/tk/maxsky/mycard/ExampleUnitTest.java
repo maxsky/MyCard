80ef70c1cc23a11a41bc26ba8cc27466fe38d651
Max Sky <maxsky@MaxSkys-Macbook-Pro.local>
2016-04-17 06:13:07 +0800

    根据 StudyJams PS1 课程制作。

---
+++ app/src/test/java/tk/maxsky/mycard/ExampleUnitTest.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2016. MaxSky.TK. All Rights Reserved.
+ */
+
+package tk.maxsky.mycard;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+	@Test
+	public void addition_isCorrect() throws Exception {
+		assertEquals(4, 2 + 2);
+	}
+}
